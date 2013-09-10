3a609de896c88e3b8314a9887e44088cdcdbf0f3
Wesley Seago <wseago2@fullsail.edu>
2013-09-10 07:37:34 -0400

    navTest

---
+++ Week2Practice/navigationControllerTest/navigationControllerTest/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  navigationControllerTest
+//
+//  Created by Wesley Seago on 9/10/13.
+//  Copyright (c) 2013 Wesley Seago. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ Week2Practice/navigationControllerTest/navigationControllerTest/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  navigationControllerTest
+//
+//  Created by Wesley Seago on 9/10/13.
+//  Copyright (c) 2013 Wesley Seago. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
