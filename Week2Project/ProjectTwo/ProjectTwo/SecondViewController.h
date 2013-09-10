6faf4f95aabc9a14961d87295529334c1f7ed590
Wesley Seago <wseago2@fullsail.edu>
2013-09-10 09:46:17 -0400

    Start project two.

---
+++ Week2Project/ProjectTwo/ProjectTwo/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  ProjectTwo
+//
+//  Created by Wesley Seago on 9/10/13.
+//  Copyright (c) 2013 Wesley Seago. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
