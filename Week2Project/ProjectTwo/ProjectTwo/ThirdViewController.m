999cd63476ff95e1b77531e0db78f0882b9f321a
Wesley Seago <wseago2@fullsail.edu>
2013-09-10 09:57:35 -0400

    Add third view controller and nav.

---
+++ Week2Project/ProjectTwo/ProjectTwo/ThirdViewController.m
@@ -0,0 +1,40 @@
+//
+//  ThirdViewController.m
+//  ProjectTwo
+//
+//  Created by Wesley Seago on 9/10/13.
+//  Copyright (c) 2013 Wesley Seago. All rights reserved.
+//
+
+#import "ThirdViewController.h"
+
+@interface ThirdViewController ()
+
+@end
+
+@implementation ThirdViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+        self.title = NSLocalizedString(@"About", @"Third");
+        self.tabBarItem.image = [UIImage imageNamed:@"second"];
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
